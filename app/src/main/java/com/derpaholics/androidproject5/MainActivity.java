3c252d448e6e87312194486d843cb2c297b23e21
manianiac <williambarronnexus@gmail.com>
2015-04-21 12:05:02 -0400

    Updated Preferences

--- app/src/main/java/com/derpaholics/androidproject5/MainActivity.java
+++ app/src/main/java/com/derpaholics/androidproject5/MainActivity.java
@@ -3,13 +3,16 @@ package com.derpaholics.androidproject5;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,12 +20,19 @@ public class MainActivity extends ActionBarActivity {
 
     private static final int MYNOTIFICATION = 1;
     private int currentNotification = MYNOTIFICATION;
-
+    SharedPreferences myPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myPreference = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean checked = myPreference.getBoolean("EnableBroadcast", true);
+        if(checked){
+            Toast.makeText(MainActivity.this, "Checked!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
